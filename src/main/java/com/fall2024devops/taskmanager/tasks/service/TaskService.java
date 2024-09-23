e4888622d75ca3e2efb0f08836114bfb5929b1ba
CYIMANA Faisal <faisal.cyimana@codeland.it>
2024-09-23 11:10:32 +0200

    Add create task endpoint.

---
+++ src/main/java/com/fall2024devops/taskmanager/tasks/service/TaskService.java
@@ -0,0 +1,46 @@
+package com.fall2024devops.taskmanager.tasks.service;
+
+import com.fall2024devops.taskmanager.common.exception.UnauthorizedException;
+import com.fall2024devops.taskmanager.common.utils.SecurityUtils;
+import com.fall2024devops.taskmanager.tasks.dto.CreateTaskDTO;
+import com.fall2024devops.taskmanager.tasks.entity.Task;
+import com.fall2024devops.taskmanager.tasks.repository.TaskRepository;
+import com.fall2024devops.taskmanager.user.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TaskService {
+
+    private final TaskRepository taskRepository;
+
+    @Autowired
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    public CreateTaskDTO.Output createTask(CreateTaskDTO.Input input) {
+        User currentUser = SecurityUtils.getCurrentUser()
+                .orElseThrow(() -> new UnauthorizedException("User not authenticated"));
+
+        Task task = Task.builder()
+                .title(input.getTitle())
+                .description(input.getDescription())
+                .status("IN_PROGRESS")
+                .user(currentUser)
+                .build();
+
+        Task savedTask = taskRepository.save(task);
+
+        return new CreateTaskDTO.Output(
+                savedTask.getId(),
+                savedTask.getTitle(),
+                savedTask.getDescription(),
+                savedTask.getStatus(),
+                savedTask.getUser().getId(),
+                savedTask.getCreatedAt(),
+                savedTask.getUpdatedAt(),
+                savedTask.getDeletedAt()
+        );
+    }
+}

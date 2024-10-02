c3e2eb8fd3e3a048a192d5bb1e4c39ea86ab24a0
jonahkayizzi <jonahkayizzi@gmail.com>
2024-10-02 10:41:32 +0200

    Add controller implementation for updating tasks

--- src/main/java/com/fall2024devops/taskmanager/tasks/controller/TaskController.java
+++ src/main/java/com/fall2024devops/taskmanager/tasks/controller/TaskController.java
@@ -1,14 +1,23 @@
 package com.fall2024devops.taskmanager.tasks.controller;
 
-import com.fall2024devops.taskmanager.common.response.GenericResponse;
-import com.fall2024devops.taskmanager.tasks.dto.CreateTaskDTO;
-import com.fall2024devops.taskmanager.tasks.dto.ListTasksDTO;
-import com.fall2024devops.taskmanager.tasks.service.TaskService;
-import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fall2024devops.taskmanager.common.response.GenericResponse;
+import com.fall2024devops.taskmanager.tasks.dto.CreateTaskDTO;
+import com.fall2024devops.taskmanager.tasks.dto.ListTasksDTO;
+import com.fall2024devops.taskmanager.tasks.dto.UpdateTaskDTO;
+import com.fall2024devops.taskmanager.tasks.service.TaskService;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/v1/tasks")
@@ -39,11 +48,12 @@ public class TaskController {
 //        return ResponseEntity.status(HttpStatus.OK).body(new GenericResponse<>("Tasks retrieved successfully", tasks));
 //    }
 //
-//    @PatchMapping("/{id}")
-//    public ResponseEntity<GenericResponse<TaskDTO.TaskDTOOutput>> updateTask(@PathVariable Long id, @RequestBody TaskDTO.TaskDTOInput taskDto) {
-//        TaskDTO.TaskDTOOutput updatedTask = taskService.updateTask(id, taskDto);
-//        return ResponseEntity.status(HttpStatus.OK).body(new GenericResponse<>("Task updated successfully", updatedTask));
-//    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<GenericResponse<UpdateTaskDTO.Output>> updateTask(@PathVariable Long id, @Valid UpdateTaskDTO.Input taskDto) {
+        UpdateTaskDTO.Output updatedTask = taskService.updateTask(id, taskDto);
+        return ResponseEntity.status(HttpStatus.OK).body(new GenericResponse<>("Task updated successfully", updatedTask));
+    }
 //
 //    @DeleteMapping("/{id}")
 //    public ResponseEntity<Void> deleteTask(@PathVariable Long id) {
